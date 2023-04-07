411eb38bd5b6cc72965b894d7962bb2a565c874f
Dmytro Skoroplias <87.dimuch@gmail.com>
2023-04-07 09:55:54 +0300

    hw-03 start

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://usrbase:ya0kcVDa4epHOTI6@cluster0.ix9tskj.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
